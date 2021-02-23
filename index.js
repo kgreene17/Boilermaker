a95d51a9ebce6c1e139f70eb02361999638ca500
Kelsey Greene <kelsey.greene17@gmail.com>
2021-02-23 11:15:35 -0700

    completeted up through React Start Script

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+
+ReactDOM.render(
+    <div>Dancing Gecko!</div>,
+    document.getElementById('app') //this is the same as my id in my index.html body div!
+)
